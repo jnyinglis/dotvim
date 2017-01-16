5b82b2bbdbc54fa7a9d308906246aff1b0b72e95
John Inglis <johninglis@Johns-Mac-mini.local>
2017-01-15 20:36:35 -0500

    Check for existing files & directories before creating symlinks

--- doit.sh
+++ doit.sh
@@ -1,1 +1,3 @@
-ln -s .vimrc ~/.vim/.vimrc
+[[ ! -a ~/.vim/.vimrc ]] && ln -s .vimrc ~/.vim/.vimrc
+[[ ! -a ~/.vim/colors ]] && mkdir ~/.vim/colors
+[[ ! -a ~/.vim/colors/mac_class.vimi ]] && ln -s $(pwd)/colors/mac_classic.vim ~/.vim/colors
