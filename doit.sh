2bc785b8c1f62a99a618453240d4a89dc46fcf59
John Inglis <johninglis@Johns-Mac-mini.local>
2017-01-28 16:18:53 -0500

    Fix test for existing link

--- doit.sh
+++ doit.sh
@@ -1,3 +1,6 @@
-[[ ! -a ~/.vim/.vimrc ]] && ln -s .vimrc ~/.vim/.vimrc
-[[ ! -a ~/.vim/colors ]] && mkdir ~/.vim/colors
-[[ ! -a ~/.vim/colors/mac_class.vimi ]] && ln -s $(pwd)/colors/mac_classic.vim ~/.vim/colors
+[[ ! -e ~/.vim/.vimrc ]] && ln -s .vimrc ~/.vim/.vimrc
+[[ ! -e ~/.vim/colors ]] && mkdir ~/.vim/colors
+[[ ! -L ~/.vim/colors/mac_classic.vim ]] && ln -s $(pwd)/colors/mac_classic.vim ~/.vim/colors
+[[ ! -e ~/.vim/plugin ]] && mkdir ~/.vim/plugin
+[[ ! -e ~/.vim/plugin/textobj ]] && mkdir ~/.vim/plugin/textobj
+[[ ! -L ~/.vim/plugin/textobj/elixirblock.vim ]] && ln -s $(pwd)/plugin/texobj/elixirblock.vim ~/.vim/plugin/textobj
